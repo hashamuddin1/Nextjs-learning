51ebb56f516154bccd432efba1757921ba47108d
hashamuddin1 <hashamlaptop@gmail.com>
2024-06-13 01:33:09 +0500

    link and navigation

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 export default function page() {
   return (
-    <div>login page</div>
-  )
+    <div>
+      login page <Link href="/about">Go To About Us Page</Link>
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <h1 className={styles.title}>Hasham</h1>
       <button onClick={handleClick}>click me</button>
+      <Link href="/login">Go to Login Page</Link>
     </div>
   );
 }
