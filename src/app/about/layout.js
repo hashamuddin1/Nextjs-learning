18a340fadca4644ebd703cb794527a5eaeeeb4ae
hashamuddin1 <hashamlaptop@gmail.com>
2024-06-15 05:42:05 +0500

    conditional layout

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,9 +1,16 @@
-
+"use client"
+import { usePathname } from "next/navigation";
 export default function Layout({ children }) {
+     const pathname = usePathname();
+     console.log(pathname);
   return (
     <div>
       <div>
-        <h2>About Us Layout</h2>
+        {pathname !== "/about/aboutschool" ? (
+          <h2>About Us Layout</h2>
+        ) : (
+          <h2>Other Layout</h2>
+        )}
       </div>
       <div>{children}</div>
     </div>
