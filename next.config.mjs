6d3d0104834b551d11542446000723b01dd215ce
hashamuddin1 <hashamlaptop@gmail.com>
2024-08-20 23:30:27 +0500

    learn about redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  redirects: async()=>{
+    return [
+    {
+      source: "/product",
+      destination: "/studentlist",
+      permanent:false // false means k product page hamne permanently delete nhi kia
+    },
+  ]
+  }
 };
 
 export default nextConfig;
